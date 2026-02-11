cd754401846a4292b435aa7afb41264673757ec1
PriyanshuKSharma <priyanshu17ks@gmail.com>
2026-02-12 01:40:43 +0530

    chore: update backend/app/models/resource.py

--- backend/app/models/resource.py
+++ backend/app/models/resource.py
@@ -8,6 +8,7 @@ class Project(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    description = Column(String, nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     created_at = Column(DateTime, default=datetime.utcnow)
     
